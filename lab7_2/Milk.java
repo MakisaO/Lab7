5223750b57b2c41f8b298baafb6acd40c3ca4296
MakisaO <pooszapc@gmail.com>
2024-08-30 12:07:47 +0700

    Add files via upload

---
+++ lab7_2/Milk.java
@@ -0,0 +1,16 @@
+package lab7_2;
+
+public class Milk extends product {
+	
+
+	public Milk(int price) {
+		super(price);
+		super.name = "Milk";
+		}
+
+//	public void setVolume(int volume) {
+//		this.volume = volume;
+//	}
+	
+	
+}
